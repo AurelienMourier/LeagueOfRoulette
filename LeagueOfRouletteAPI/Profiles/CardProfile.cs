523d19176513b8d3fee6bb4eadec0504d0601679
aurelien <a.mourier@ec2i.fr>
2022-05-20 09:43:15 +0200

    fix dto card

--- LeagueOfRouletteAPI/Profiles/CardProfile.cs
+++ LeagueOfRouletteAPI/Profiles/CardProfile.cs
@@ -17,7 +17,13 @@ namespace LeagueOfRouletteAPI.Profiles
             CreateMap<Card, CardDto>()
                .ForMember(dest => dest.RarityCard, opt => opt.MapFrom(src => src.RarityCard.Name))
                .ForMember(dest => dest.TypeCard, opt => opt.MapFrom(src => src.TypeCard.Name))
-               .ForMember(dest => dest.StatCard, opt => opt.MapFrom(src => src.StatCard));
+               .ForMember(dest => dest.StatCard, opt => opt.MapFrom(src => src.StatCard))
+               .ForMember(dest => dest.StatCard.AD, opt => opt.MapFrom(src => src.StatCard.AD))
+               .ForMember(dest => dest.StatCard.AP, opt => opt.MapFrom(src => src.StatCard.AP))
+               .ForMember(dest => dest.StatCard.AR, opt => opt.MapFrom(src => src.StatCard.AR))
+               .ForMember(dest => dest.StatCard.AS, opt => opt.MapFrom(src => src.StatCard.AS))
+               .ForMember(dest => dest.StatCard.MR, opt => opt.MapFrom(src => src.StatCard.MR))
+               .ForMember(dest => dest.StatCard.MV, opt => opt.MapFrom(src => src.StatCard.MV));
         }
     }
 }
