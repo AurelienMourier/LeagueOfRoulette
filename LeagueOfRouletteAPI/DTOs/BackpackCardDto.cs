d1c5e928a3ab474641808983502a5d73eda01dd4
aurelien <a.mourier@ec2i.fr>
2022-05-21 14:05:10 +0200

    Adding BackpackCardProfile

--- LeagueOfRouletteAPI/DTOs/BackpackCardDto.cs
+++ LeagueOfRouletteAPI/DTOs/BackpackCardDto.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace LeagueOfRouletteAPI.Controllers
+namespace LeagueOfRouletteAPI.DTOs
 {
     public class BackpackCardDto
     {
---
+++ LeagueOfRouletteAPI/Profiles/BackpackCardProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using LeagueOfRouletteAPI.DTOs;
+using LeagueOfRouletteAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeagueOfRouletteAPI.Profiles
+{
+    public class BackpackCardProfile : Profile
+    {
+        public BackpackCardProfile()
+        {
+            CreateMap<BackpackCardDto, BackpackCard>();
+        }
+    }
+}
